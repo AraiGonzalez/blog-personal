4d64dba0d44c031d3a1840044520df71fac5cd2c
Arai Gonzalez <araiestefania94@gmail.com>
2024-11-24 19:01:40 -0300

    documentacion

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -1,15 +1,15 @@
 const request = require('supertest');
-const { app, server } = require('../app'); // Importa la aplicación y el servidor
-const { connectDB, closeDB } = require('../config/jest.setup'); // Importa las funciones de conexión
+const { app, server } = require('../index'); 
+const { connectDB, closeDB } = require('../config/jest.setup'); 
 const User = require('../models/user'); // Importa el modelo de usuario
 beforeAll(async () => {
   await connectDB(); // Conéctate a la base de datos en memoria antes de ejecutar las pruebas
 });
 
 afterAll(async () => {
-  await closeDB(); // Cierra la conexión después de que todas las pruebas se hayan ejecutado
+  await closeDB(); // Cierra la conexión 
   if (server && server.close) {
-    server.close(); // Cierra el servidor después de que todas las pruebas se hayan ejecutado
+    server.close(); // Cierra el serv
   }
 });
 
@@ -23,13 +23,13 @@ describe('Auth Routes', () => {
     const response = await request(app)
       .post('/auth/register')
       .send(userData)
-      .expect(302); // Verifica que haya una redirección después del registro
+      .expect(302); 
 
-    expect(response.headers.location).toBe('/auth/login'); // Verifica que redirige al login
+    expect(response.headers.location).toBe('/auth/login'); 
 
     const user = await User.findOne({ username: userData.username });
-    expect(user).not.toBeNull(); // Verifica que el usuario se haya creado
-    expect(user.role).toBe('user'); // Verifica que el rol sea 'user' por defecto
+    expect(user).not.toBeNull(); 
+    expect(user.role).toBe('user'); 
   });
 
   test('should create an admin user', async () => {
@@ -41,10 +41,10 @@ describe('Auth Routes', () => {
     const response = await request(app)
       .post('/auth/create-admin')
       .send(adminData)
-      .expect(201); // Verifica que se crea correctamente
-    // Verifica que la respuesta contenga el mensaje esperado
+      .expect(201); 
+    
     expect(response.body.message).toBe('Usuario administrador creado exitosamente');
-    // Verifica que el usuario administrador se haya creado en la base de datos
+    
     const adminUser = await User.findOne({ username: adminData.username });
     expect(adminUser).not.toBeNull();
     expect(adminUser.role).toBe('admin'); // Verifica que el rol sea 'admin'
